e4e922b0483ee63b2e4bc842138941188592e40d
Serkan <serkanbulutgenc@gmail.com>
2025-04-24 20:28:34 +0300

    Day 5

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,32 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import altair as alt
 
 
 def main():
-    st.write("Hello World")
+    st.header("st.write")
+    # Example 1
+    st.write("Hello, *World!* :sunglasses:")
+
+    # Example 2
+    st.write(1234)
+
+    # Example 3
+    df = pd.DataFrame({"first_col": [1, 2, 3, 4], "second_col": [10, 20, 30, 40]})
+    st.write(df)
+
+    # Example 4
+    st.write("Below is a Dataframe:", df, "Above is a dataframe.")
+
+    # Example 5
+    df2 = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+    c = (
+        alt.Chart(df2)
+        .mark_circle()
+        .encode(x="a", y="b", size="c", color="c", tooltip=["a", "b", "c"])
+    )
+    st.write(c)
 
 
 if __name__ == "__main__":
