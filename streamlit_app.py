1d624fd79543cd4b4f40a2f23e724750d432e534
Serkan <serkanbulutgenc@gmail.com>
2025-05-13 22:12:27 +0300

    Day 17

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,24 +2,9 @@ import streamlit as st
 
 
 def main():
-    st.title("Customizing the theme of Streamlit apps")
+    st.title("st.secrets")
 
-    st.write("Contents of the `.streamlit/config.toml` file of this app")
-
-    st.code(
-        """
-            [theme]
-            primaryColor="#F39C12"
-            backgroundColor="#2E86C1"
-            secondaryBackgroundColor="#AED6F1"
-            textColor="#FFFFFF"
-            font="monospace"
-            """,
-        line_numbers=True,
-    )
-
-    number = st.sidebar.slider("Select a number", 0, 10, 5)
-    st.write("Selected number from slider widget is:", number)
+    st.write(st.secrets["message"])
 
 
 if __name__ == "__main__":
