4105560069f792613263468779a24cc8e893002d
Serkan <serkanbulutgenc@gmail.com>
2025-05-13 21:18:28 +0300

    Day 9

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,14 @@
 import streamlit as st
-from datetime import datetime, time
+import numpy as np
+import pandas as pd
 
 
 def main():
-    st.header("st.slider")
-    # Example 1
-    st.subheader("Slider")
+    st.header("Lİne Chart")
 
-    age = st.slider("How old are you?", 0, 130, 25)
-    st.write("I'am ", age, "years old.")
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
-    # Example 2
-    st.subheader("Range slieder")
-
-    values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
-    st.write("values", values)
-
-    # Example 3
-    st.subheader("Range time slider")
-    appointment = st.slider(
-        "Schedule your appoinmtment:", value=(time(11, 30), time(12, 45))
-    )
-    st.write("You scheduled for:", appointment)
-
-    # Example 4
-    st.subheader("Datetime slider")
-    start_time = st.slider(
-        "When dou you start?",
-        value=datetime(2020, 1, 1, 9, 30),
-        format="MM/DD/YY - hh:mm",
-    )
-    st.write("Start time", start_time)
+    st.line_chart(chart_data, x_label="X label", y_label="Y label")
 
 
 if __name__ == "__main__":
