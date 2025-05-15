4c9d9af2ef435ece6137fcb6f69266931956fa70
Serkan <serkanbulutgenc@gmail.com>
2025-05-15 22:09:16 +0300

    Day 18

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,22 @@
 import streamlit as st
+import pandas as pd
 
 
 def main():
-    st.title("st.secrets")
+    st.title("st.file_uploader")
 
-    st.write(st.secrets["message"])
+    st.subheader("Input Csv")
+
+    uploaded_file = st.file_uploader("Choose a file", type=["csv", "xlsx"])
+
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.subheader("DataFrame")
+        st.write(df)
+        st.subheader("Descriptive statistics")
+        st.write(df.describe())
+    else:
+        st.info("Upload a CSV file")
 
 
 if __name__ == "__main__":
