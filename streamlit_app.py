7438ccc644e79b0dfc8fa95947e562e9f7f5b00e
Serkan <serkanbulutgenc@gmail.com>
2025-05-15 22:21:27 +0300

    Day 21

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,23 @@
 import streamlit as st
 import pandas as pd
+import time
 
 
 def main():
-    st.set_page_config(layout="wide")
-
-    st.title("How to layout your Streamlit app")
+    st.title("st.progress")
 
     with st.expander("About this app"):
         st.write(
-            "This app shows the various ways on how you can layout your streamlit app"
-        )
-        st.image(
-            "https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png",
-            width=250,
+            "You can now display the progress of your calculations in a Streamlit app with the `st.progress`"
         )
 
-    st.sidebar.header("Input")
-    username = st.sidebar.text_input("What is your name?")
-    user_emoji = st.sidebar.selectbox(
-        "Choose an emoji", ["", "😄", "😆", "😊", "😍", "😴", "😕", "😱"]
-    )
-    user_food = st.sidebar.selectbox(
-        "What is your favorite food?",
-        ["", "Tom Yum Kung", "Burrito", "Lasagna", "Hamburger", "Pizza"],
-    )
+    my_bar = st.progress(0)
 
-    st.header("Output")
+    for percent_complete in range(100):
+        time.sleep(0.05)
+        my_bar.progress(percent_complete + 1)
 
-    col1, col2, col3 = st.columns(3)
-
-    with col1:
-        if username != "":
-            st.write(f" Hello {username}")
-        else:
-            st.write("Please enter your **name**")
-
-    with col2:
-        if user_emoji != "":
-            st.write(f"{user_emoji} is your favorite **emoji**!")
-        else:
-            st.write("👈 Please choose an **emoji**!")
-
-    with col3:
-        if user_food != "":
-            st.write(f"🍴 **{user_food}** is your favorite **food**!")
-        else:
-            st.write("👈 Please choose your favorite **food**!")
+    st.balloons()
 
 
 if __name__ == "__main__":
